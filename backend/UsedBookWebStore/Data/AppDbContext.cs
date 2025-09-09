9e84d81ba3591a054e48bb24420baf19435af87a
Nagy János <batyus4563@gmail.com>
2025-09-09 15:48:02 +0200

    backend, models,locale database ,apis

---
+++ backend/UsedBookWebStore/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UsedBookWebStore.Models;
+
+namespace UsedBookWebStore.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
